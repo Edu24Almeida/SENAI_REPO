8867495bf750f1fc6128b3f28cfa93d856a9852c
Edu24Almeida <edu24estudos@gmail.com>
2025-06-17 19:52:14 -0300

    Add files via upload

---
+++ aula_16.06_Try_and_except_ex03.py
@@ -0,0 +1,74 @@
+# def teste():
+#     try:
+#         numero = int(input('Numero: '))
+# #         print(numero)
+# #     except TypeError as erro:
+# #         print(erro) 
+# #     except ValueError  as erro:
+# #         print(erro)         
+# #     else:
+# #         print('Erro não identificado')
+# #     finally:
+# #         print('Fim de carregamento... ')    
+# # teste()
+
+
+# # # numero = int(input('Numero: '))
+# # # print(numero)
+
+
+
+# def div():
+#   try:  
+#     n1  =  int(input('Numero 1:'))
+#     n2  =  int(input('Numero 1:'))
+#     d = n1 / n2
+#   except ZeroDivisionError as erro:
+#     print('Erro',erro)
+#   except ValueError as erro:
+#     print(erro)
+# #   else:
+# #     print('Erro não identificado') 
+
+
+#   finally:
+#    print('fim de carregamento')
+
+
+# div()
+
+
+# def ativida3():
+#    try: 
+#     lista = [1,2,3]
+#     indice = int(input('indice: '))
+#     lista[indice]
+#    except IndexError as erro:
+#      print(erro)
+#    except ValueError as erro:
+#      print(erro)
+#    else:
+#      print(lista[indice])   
+#    finally:
+#      print('Fim de carregamento...')  
+
+
+# ativida3()     
+# # l = [1,2,3]
+# # print(l[5])
+
+
+# bug = não 
+# erro = 
+
+
+n = input('escolha: ')
+x = 2
+
+
+if x == n:
+  print('teste')
+
+
+else:
+  print('x')  
